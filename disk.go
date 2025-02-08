1bedcf6556e4276e86c90b4539c58a995190190e
jitianrui <8620446+jitianrui@user.noreply.gitee.com>
2025-02-08 13:42:26 +0800

    1

--- disk.go
+++ disk.go
@@ -2,63 +2,76 @@ package disk
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"golang.org/x/crypto/ssh"
-	"log"
 	"strconv"
 	"strings"
 )
 
-func Hello() {
-	fmt.Println("Hello from mylibrary!")
-}
-
 var (
 	stdoutBuf bytes.Buffer
 	stderrBuf bytes.Buffer
+	total     int = 10 * 1024 * 1024
 )
 
-func sshd(d string) {
+type Config struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	Hard     string
+}
+
+type Directory struct {
+	Size int
+}
+
+func InitSSH(c *Config) (Directory, error) {
+	var tech Directory
 	config := &ssh.ClientConfig{
-		User:            "root",
-		Auth:            []ssh.AuthMethod{ssh.Password("123.com")},
+		User:            c.User,
+		Auth:            []ssh.AuthMethod{ssh.Password(c.Password)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-	client, err := ssh.Dial("tcp", "192.168.1.30:9200", config)
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", c.Host, &c.Port), config)
 	if err != nil {
-		log.Fatalf("Failed to dial: %s", err)
-		return
+		return Directory{}, err
 	}
 	defer client.Close()
 	session, err := client.NewSession()
 	if err != nil {
-		log.Fatalf("Failed to create session: %s", err)
+		return Directory{}, err
 	}
 	defer session.Close()
 	session.Stdout = &stdoutBuf
 	session.Stderr = &stderrBuf
-	err = session.Run(fmt.Sprintf("df -k %s | awk 'NR==2 {print $4}'", d)) // 替换为你想要执行的命令
+	err = session.Run(fmt.Sprintf("df -k %s | awk 'NR==2 {print $4}'", c.Hard)) // 替换为你想要执行的命令
 	if err != nil {
-		log.Fatalf("执行命令失败: %v", err)
+		return Directory{}, err
 	}
 
 	available, err := strconv.Atoi(strings.TrimSpace(stdoutBuf.String()))
-	total := 10 * 1024 * 1024
 	if err != nil {
-		log.Fatalf("解析错误%v", err)
-		return
+		return Directory{}, err
 	}
-	if available > total {
-		fmt.Println("可用硬盘大于10G")
-	} else {
-		fmt.Println("可用硬盘小于10G")
-	}
-	fmt.Printf("可用硬盘大小当前为%dKB\n", available)
+	tech = Directory{Size: available}
+	return tech, nil
+	//if available > total {
+	//	fmt.Println("可用硬盘大于10G")
+	//} else {
+	//	fmt.Println("可用硬盘小于10G")
+	//}
+	//fmt.Printf("可用硬盘大小当前为%dKB\n", available)
 }
 
-func main() {
-	config := flag.String("disk", "/", "a string")
-	flag.Parse()
-	sshd(*config)
+func (t Directory) GB() int {
+	return t.Size / 1024 / 1024
+}
+
+func (t Directory) KB() int {
+	return t.Size
+}
+
+func (t Directory) MB() int {
+	return t.Size / 1024
 }
